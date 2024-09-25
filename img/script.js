18bf69390b990190bd2b1c05590f6ea499409a4d
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2024-09-25 10:51:15 -0300

    1.0

--- img/script.js
+++ img/script.js
@@ -7,3 +7,11 @@ const datos = { //coonstante llamada de datos
 };
 
 const divDatos = document.getElementById('datos-empresa');
+
+divDatos.innerHTML = '
+	<h2>Datos de la empresa</h2>
+	<p><strong>Nombre:</strong>${datos.nombre}</p>
+	<p><strong>Dirección:</strong>${datos.direccion}</p>
+	<p><strong>Teléfono:</strong>${datos.telefono}</p>
+	<p><strong>Email:</strong><a href="mailto:${datos.email}">${datos.email}</a></p>
+	<p><strong>Sitio Web:</strong><a href="https://github.com/dashboard" target="_blank">${datos.website}</a></p>';
