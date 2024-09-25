70b02b0d9d52cc2453c2e3105c76c764ec3821fc
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2024-09-25 10:36:46 -0300

    1.0

--- img/script.js
+++ img/script.js
@@ -1,5 +1,9 @@
 const datos = { //coonstante llamada de datos
 	nombre: "ElPapu Entertainment",
 	direccion: "Los Desarrolladores #12821, Santiago",
-	telefono: ""
+	telefono: "+56 9 33537787",
+	email: "soporte@elpapuentertainment.cl",
+	website: "www.elpapuentertainment.cl"
 };
+
+const divDatos = document.getElementById('datos-empresa');
