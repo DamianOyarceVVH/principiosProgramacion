1b4f72d9de4d8b64d1e6a3945c5e77954ae05b57
DamianOyarceVVH <damianoyarce@liceovvh.cl>
2024-09-25 10:31:58 -0300

    1.1

--- Script.js
+++
@@ -1,3 +0,0 @@
-document.write ('Bienvenido Estudiante');
-document.write ('<br>');
-document.write ('Hasta Luego Estudiantes');
---
+++ img/script.js
@@ -0,0 +1,5 @@
+const datos = { //coonstante llamada de datos
+	nombre: "ElPapu Entertainment",
+	direccion: "Los Desarrolladores #12821, Santiago",
+	telefono: ""
+};
